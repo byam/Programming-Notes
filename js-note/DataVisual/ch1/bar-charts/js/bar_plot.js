2b432a5153794ab02d83f1a69b574cb1d1002d76
byam <byambaa.mng@gmail.com>
2015-12-08 02:52:37 +0900

    js visual

--- js-note/DataVisual/ch1/bar-charts/js/bar_plot.js
+++ js-note/DataVisual/ch1/bar-charts/js/bar_plot.js
@@ -1,14 +1,38 @@
-var wins = [[[2006,13],[2007,11],[2008,15],[2009,15],[2010,18],[2011,21],
-             [2012,28]]];
+var wins = [[[0,28]],[[1,28]],[[2,21]],[[3,20]],[[4,19]]];
+var teams = [
+    [0, "MCI"],
+    [1, "MUN"],
+    [2, "ARS"],
+    [3, "TOT"],
+    [4, "NEW"]
+];
 
-window.onload = function (){
-    Flotr.draw(
-        document.getElementById("chart"),
-        wins,
-        {
-            bars: {
-                show: true
-            }
+window.onload = function () {
+    Flotr.draw(document.getElementById("chart"), wins, {
+        title: "Premier League Wins (2011-2012)",
+
+        colors: ["#89AFD2", "#1D1D1D", "#DF021D", "#0E204B", "#E67840"],
+
+        bars: {
+            show: true,
+            barWidth: 0.5,
+            shadowSize: 0,
+            fillOpacity: 1,
+            lineWidth: 0
+        },
+
+        yaxis: {
+            min: 0,
+            tickDecimals: 0
+        },
+
+        xaxis: {
+            ticks: teams
+        },
+
+        grid: {
+        horizontalLines: false,
+        verticalLines: false
         }
-    );
+    });
 };
